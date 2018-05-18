62a4ed8134ce75e5f800280ce6ed17afe406e0f5
佐藤大介 <workshift56@gmail.com>
2018-05-18 22:45:38 +0900

    first commit

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
   "log"
   "net/http"
+  "sync"
+	"text/template"
+  "path/filepath"
 )
 
-func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.write([]byte(`
-      <html>
-        <head>
-          <title>チャット</title>
-        </head>
-      </html>
-      `))
+type templateHandler struct {
+  once sync.Once
+  filename string
+  templ *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  t.once.Do(func() {
+    t.templ =
+
+    template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
   })
+  t.templ.Execute(w, nil)
+}
+
+func main() {
+  http.Handle("/", &templateHandler{filename: "chat.html"})
+  // Webサーバーの開始
+  if err := http.ListenAndServe(":8080",nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
