a6e0788b55e3e509e9cc250de09bed82c14ba7b3
佐藤大介 <workshift56@gmail.com>
2018-05-21 14:52:50 +0900

    チャットルームの生成と利用

--- main.go
+++ main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "sync"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
 	"text/template"
-  "path/filepath"
 )
 
 type templateHandler struct {
-  once sync.Once
-  filename string
-  templ *template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
 // ServeHTTPはHTTPリクエストを処理
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  t.once.Do(func() {
-    t.templ =
+	t.once.Do(func() {
+		t.templ =
 
-    template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-  })
-  t.templ.Execute(w, nil)
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-  http.Handle("/", &templateHandler{filename: "chat.html"})
-  // Webサーバーの開始
-  if err := http.ListenAndServe(":8080",nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルームを開始
+	go r.run()
+	// Webサーバーの開始
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
